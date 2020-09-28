62583e4650e4673de1a70154c3be17bfc0b68b56
swepz <taza@live.se>
2020-09-28 08:23:16 +0200

    blink example LED: 1 -> 4

---
+++ example/src/main.c
@@ -0,0 +1,31 @@
+// Core library for code-sense
+#if defined(ENERGIA)
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "inc/hw_memmap.h"
+#else  // error
+#error Platform not defined
+#endif
+
+void setup() {
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION | SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1 | GPIO_PIN_0);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0);
+}
+
+void loop() {
+    static uint32_t delay_time = 1000000;
+    // Turn on LED 1 -> 4
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1 | GPIO_PIN_0, GPIO_PIN_1 | GPIO_PIN_0);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0, GPIO_PIN_4 | GPIO_PIN_0);
+    SysCtlDelay(delay_time);
+    // Turn off LED 1 -> 4
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1 | GPIO_PIN_0, 0);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0, 0);
+    SysCtlDelay(delay_time);
+}
